53d092db14e63352398b0e0b548176556bf9f6ae
biryk <busterm@yandex.ru>
2018-01-27 23:35:33 +0300

    commit

--- init.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-
-def getconfig():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    slack_webhook_url = config['SLACK']['slack_webhook_url']
-    admin_mail = config['DEFAULT']['admin_mail']
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from modules.getconfig import getconfig
+from modules.slack import Mods
+import configparser
 
 
 def main():
-    getconfig()
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    slack_webhook_url = config['SLACK']['slack_webhook_url']
+    Mods.slack(slack_webhook_url, "kukuku", "tv1", ":one:")
 
 
 if __name__ == "__main__":
