cbc34198de671500b0910ad172efdc73c484ba29
biryk <busterm@yandex.ru>
2018-01-29 18:36:42 +0300

    add disk checker

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from modules.slack import Mods
+from modules.diskspace import alertdisk
 import configparser
 
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+slack_webhook_url = config['SLACK']['slack_webhook_url']
+hostname = ['DEFAULT']['hostname']
+icon = ['DEFAULT']['icon']
+admin_mail = config['DEFAULT']['admin_mail']
+
+
 def main():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    slack_webhook_url = config['SLACK']['slack_webhook_url']
-    Mods.slack(slack_webhook_url, "kukuku", "tv1", ":one:")
+    if alertdisk() == 1:
+        Mods.slack(slack_webhook_url, "Alert disk on host", hostname, icon)
+    else:
+        pass
 
 
 if __name__ == "__main__":
         main()
+
--- modules/slack.py
+++ modules/slack.py
@@ -2,15 +2,12 @@ import requests
 import json
 
 
-def slack(hook_url, message, host, emoji):
-
-    data = {
-        'text': message,
-        'username': host,
-        'icon_emoji': emoji
-    }
-
-    response = requests.post(hook_url, data=json.dumps(
-        data), headers={'Content-Type': 'application/json'})
-
-    return response.status_code
+class Mods:
+    @staticmethod
+    def slack(hook_url, message, host, emoji):
+        data = {
+            'text': message,
+            'username': host,
+            'icon_emoji': emoji
+            }
+        requests.post(hook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
