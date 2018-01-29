beaaaaf041120264036951065585b951bc9f781c
biryk <busterm@yandex.ru>
2018-01-29 18:37:12 +0300

    add disk checker

---
+++ modules/diskspace.py
@@ -0,0 +1,23 @@
+import os
+
+
+def getdiskfree():
+    s = os.statvfs('/')
+    d = (s.f_bavail * s.f_frsize)
+    return d
+
+
+def humanbytes(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0:
+            return "%3.1f%s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+
+
+def alertdisk():
+    free = getdiskfree()
+    if free >= 5368709120:
+        return 1
+    else:
+        return 0
