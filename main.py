3b32364d8286a21efba407e1b59b6a702c423fbe
biryk <busterm@yandex.ru>
2018-01-30 13:22:04 +0300

    commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from classes.modules.slack import Mods
-from classes.modules.diskspace import alertdisk
+from classes.modules.slack import MainSlack
+from classes.modules.diskspace import MainDiskSpace
 from classes.core import Main
 
 
 def main():
-    if alertdisk() != 0:
-        Mods.slack(Main.slack_webhook_url, "Alert disk on host " + Main.hostname + \
-                   ", now available: " + str(alertdisk()), Main.hostname, Main.icon)
+    if MainDiskSpace().alertdisk() != 0:
+        MainSlack.slack(Main.slack_webhook_url, "Alert disk on host " + Main.hostname + \
+                   ", now available: " + str(MainDiskSpace().alertdisk()), Main.hostname, Main.icon)
     else:
         pass
 
