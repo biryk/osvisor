8c4180795da686a6ea1048c02a3e71aceafd4ea8
biryk <busterm@yandex.ru>
2018-01-31 13:25:03 +0300

    commit

--- classes/core.py
+++ classes/core.py
@@ -5,7 +5,8 @@ class Main:
     config = configparser.ConfigParser()
     config.read('config.ini')
     slack_webhook_url = config['SLACK']['slack_webhook_url']
-    admin_mail = config['DEFAULT']['admin_mail']
     hostname = config['SLACK']['hostname']
     icon = config['SLACK']['icon']
+    hosts = list(config.items('HOSTS'))
+
 
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from classes.modules.slack import MainSlack
 from classes.modules.diskspace import MainDiskSpace as aler
+from classes.modules.checkhosts import MainRequest
 from classes.core import Main
 
 
 def main():
     if aler().alertdisk() != 0:
-        MainSlack.slack(Main.slack_webhook_url, "Alert disk on host " + Main.hostname + \
-                   ", now available: " + str(aler().alertdisk()), Main.hostname, Main.icon)
+        MainSlack.slack(Main.slack_webhook_url, "Alert disk on host " + Main.hostname +
+                        ", now available: " + str(aler().alertdisk()), Main.hostname, Main.icon)
     else:
         pass
+    MainRequest().sort_host()
 
 
 if __name__ == "__main__":
